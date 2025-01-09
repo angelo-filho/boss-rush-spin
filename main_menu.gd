ebbe9e383036a6495da465c142382b27d4d2a156
jvvitorcs <jvvitorcs97@gmail.com>
2025-01-09 13:24:17 -0300

    Adding main menu scene base

---
+++ main_menu.gd
@@ -0,0 +1,13 @@
+extends Control
+
+func _on_play_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://levels/player_test.tscn")
+	pass
+
+
+func _on_options_button_pressed() -> void:
+	pass
+
+
+func _on_exit_pressed() -> void:
+	get_tree().quit()
